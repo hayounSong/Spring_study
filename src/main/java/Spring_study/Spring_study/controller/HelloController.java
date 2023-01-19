c6941f1c3288e28e8b23b84317b33968a9b6ae73
hayoun <fishingest@kookmin.ac.kr>
2023-01-20 02:00:00 +0900

    Spring MVC

--- src/main/java/Spring_study/Spring_study/controller/HelloController.java
+++ src/main/java/Spring_study/Spring_study/controller/HelloController.java
@@ -3,11 +3,18 @@ package Spring_study.Spring_study.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
 public class HelloController {
     @GetMapping("hello")
     public String hello(Model model){
         model.addAttribute("data","spring");
         return "hello";
+    }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name")String name,Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
     }
 }
