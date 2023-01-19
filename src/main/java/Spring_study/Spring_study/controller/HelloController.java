8f2ae4dc13f492c9e60aa0bbec35f73155e3340f
hayoun <fishingest@kookmin.ac.kr>
2023-01-20 02:14:01 +0900

    Spring API

--- src/main/java/Spring_study/Spring_study/controller/HelloController.java
+++ src/main/java/Spring_study/Spring_study/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -17,4 +18,31 @@ public class HelloController {
         model.addAttribute("name",name);
         return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello "+name;
+    }
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello=new Hello();
+        hello.setName(name);
+        return hello;
+        //객체를 반환하면 json 형식으로 된다
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
