2617532aa8ce51dfd7d5f5f86f687bb2cf5d8036
hayoun <fishingest@kookmin.ac.kr>
2023-01-21 15:17:13 +0900

    회원 서비스 개발

---
+++ src/main/java/Spring_study/Spring_study/service/MemberService.java
@@ -0,0 +1,35 @@
+package Spring_study.Spring_study.service;
+
+import Spring_study.Spring_study.domain.Member;
+import Spring_study.Spring_study.repository.MemberRepository;
+import Spring_study.Spring_study.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository=new MemoryMemberRepository();
+
+    public Long join(Member member){
+        //중복 회원 방지
+         validateDuplicateMember(member);
+         //컨트롤 t로 리팩토링 메서드 불러오는거 가능하다, 이렇게 해줄 수 있었음, extract method
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+           .ifPresent(m->{
+           //result가 null이 아니면 동작
+           throw new IllegalStateException("이미 존재하는 회원입니다.");
+       });
+    }
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
