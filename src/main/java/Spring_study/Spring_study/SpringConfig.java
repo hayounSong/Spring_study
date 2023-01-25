c4e5330c346d9d826762d43a54418e97be1a3eb8
hayoun <fishingest@kookmin.ac.kr>
2023-01-26 02:30:40 +0900

    Spring Data JPA

--- src/main/java/Spring_study/Spring_study/SpringConfig.java
+++ src/main/java/Spring_study/Spring_study/SpringConfig.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-
 @Configuration
 public class SpringConfig {
 
@@ -17,23 +15,31 @@ public class SpringConfig {
 //        this.dataSource = dataSource;
 //    }
 
-    @Autowired
-    private EntityManager em;
+//    @Autowired
+//    private EntityManager em;
+//
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    private final MemberRepository memberRepository;
+@Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
 
     @Bean
 
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
+//    @Bean
+//    public MemberRepository memberRepository(){
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//        return new
+//    }
 }
