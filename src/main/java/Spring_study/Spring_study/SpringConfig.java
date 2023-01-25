fa80856988ab7fc22e811a79dcb70d694a57e85a
hayoun <fishingest@kookmin.ac.kr>
2023-01-26 00:42:49 +0900

    Spring Jdbc

--- src/main/java/Spring_study/Spring_study/SpringConfig.java
+++ src/main/java/Spring_study/Spring_study/SpringConfig.java
@@ -1,14 +1,24 @@
 package Spring_study.Spring_study;
 
-import Spring_study.Spring_study.domain.Member;
+import Spring_study.Spring_study.repository.JdbcMemberRepository;
 import Spring_study.Spring_study.repository.MemberRepository;
 import Spring_study.Spring_study.repository.MemoryMemberRepository;
 import Spring_study.Spring_study.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
 
     public MemberService memberService(){
@@ -17,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
